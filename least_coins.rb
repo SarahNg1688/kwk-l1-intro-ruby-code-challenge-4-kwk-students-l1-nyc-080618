deb314d706c09aa21b031f79e0fe319d88674735
Sarah Ng <agbtr101a@gmail.com>
2018-08-09 13:25:28 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,9 +1,15 @@
 #write out your code here
 
-def least_coins(cents)
+def least_coins(coins)
  money_hash = {:quarters => "0", :dimes => "0", :nickels => "0", :pennies => "0"} 
-quarters = coins/25 = 3 
-money_hash[:quarters] 
+quarters = coins/25  
+money_hash[:quarters] = quarters
+coins = coins % 25
+dimes = coins/10 
+money_hash[:dimes] = dimes
+coins = coins % 10
+nickels = coins/5 
+money_hash[:nickels] = nickels
 
 end
 puts least_coins(98)
